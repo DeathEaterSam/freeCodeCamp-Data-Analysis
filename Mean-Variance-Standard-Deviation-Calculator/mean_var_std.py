187ebf4817ac1182a1aa6e275c8ab8adda0ae0ed
Sam <87801374+DeathEaterSam@users.noreply.github.com>
2024-08-04 21:44:14 +0500

    Added my implementation

--- Mean-Variance-Standard-Deviation-Calculator/mean_var_std.py
+++ Mean-Variance-Standard-Deviation-Calculator/mean_var_std.py
@@ -1,8 +1,15 @@
 import numpy as np
 
 def calculate(list):
-
-
-
+    if len(list) < 9:
+        raise ValueError("List must contain nine numbers.")
+    arr = np.array(list).reshape(3,3)
+    np.mean.__name__ = 'mean'
+    np.var.__name__ = 'variance'
+    np.std.__name__ = 'standard deviation'
+    np.min.__name__ = 'min'
+    np.max.__name__ = 'max'
+    np.sum.__name__ = 'sum'
+    calculations = {fun.__name__: [fun(arr,axis=0).tolist(),fun(arr,axis=1).tolist(),fun(arr)] for fun in [np.mean, np.var, np.std, np.min, np.max, np.sum]}
 
     return calculations
